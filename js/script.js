2c4fc923fd59f06ebfb15738081b04e60036660f
kevingil <keven.gs@gmail.com>
2013-02-27 19:00:30 -0800

    added some fancy js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	var face_lis = $("#faces li");
+	$.each(face_lis, function(i, v){
+		$(v).mouseover(function(){
+
+
+
+		});
+
+
+		$(v).mouseout(function(){
+			
+
+		});				
+	});	
+});	
