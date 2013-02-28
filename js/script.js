ac390ed020bdefcac1d6eec4492b7942e994bbf8
Gabe <gabrield@bavc.org>
2013-02-27 18:53:16 -0800

    Border

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
-	var face_lis = $("#faces li");
-	$.each(face_lis, function(i, v){
+	$.each($("#faces li a"), function(i, v){
 		$(v).mouseover(function(){
-			$("#faces li a").css({
-				"background-image" : "url('images/" + v.className + ".jpg')",
+			$("#faces li").css({
+				"background-image" : "url('images/" + v.parentNode.className + ".jpg')",
 				"background-attachment" : "fixed",
 				"background-repeat" : "none"
 			});
 		});
 		$(v).mouseout(function(){
-			$("#faces li a").css({
-				"background" : "inherit"
+			$.each($("#faces li"), function(i, v){
+				$(v).css({
+					"background-image" : "url('images/" + v.className + ".jpg')",
+					"background-attachment" : "fixed",
+					"background-repeat" : "none"
+				});
+			
 			});
+			
 		});				
 	});	
 });	
