96b68c56e961938e70bb047263756d8450630ad1
Gabe <gabrield@bavc.org>
2013-03-04 18:42:09 -0800

    Added shuffle feature and comments.

--- js/script.js
+++ js/script.js
@@ -1,65 +1,82 @@
+// list of all students, teachers
+var members = [
+	"amy", 
+	"andrew", 
+	"angelisa", 
+	"bianca", 
+	"bradley", 
+	"dwana", 
+	"gabe", 
+	"jon", 
+	"karina", 
+	"kevin", 
+	"koby", 
+	"my"
+];
 
-var students = [];
 
-	var s = [];
-
+// what happens when the page is ready
 $(document).ready(function() {
-
-
-
-
 
 	// reset
 	$('#heading a').click(function() {
+		// empty the profile element
+		$('#profile').empty();
+		// generate new faces
+		generateFaces();
+	});
 
-		$("#faces li a").css({
-			"background" : "inherit"
+	// generate faces faces
+	generateFaces();
+});	
+
+// generate faces function
+var generateFaces = function() {
+	
+	// create a variable for the faces element
+	var base_elem = $('#faces');
+
+	// create a variable for the profile element
+	var profile_elem = $('#profile')	
+
+	// shuffle faces
+	members.sort(function() { return 0.5 - Math.random(); });
+
+	// empty base element
+	base_elem.empty();
+
+	// loop through each of the 'members' array
+	$(members).each(function(i, v) {
+		// create an element that we will fill with content for each face
+		var elem = $('<li><a href="#"></a></li>');
+
+		// set all elements to have a unique background image based on the members array
+		elem.css({
+			"background-image" : "url('images/" + v + ".jpg')",			
 		});
 
-		$('#profile').empty();
+		// find the <a> element in the elem, and assign a click event
+		elem.find('a').click(function(e){
 
-	});
-
-
-
-	// randomize
-	$('#shuffle').click(function() {
-		console.log('yeah');
-	});
-
-
-
-
-
-	// assign events
-	$.each($("#faces li"), function(i, v) {
-
-		s.push(v.className);
-		
-		var link = $(v).find('a');
-
-		$(link).click(function(e) {
-
-
-
-			var json_file = "data/" + v.className + ".json";
-
-			$.getJSON( json_file, function(data) {
+			// get the associated data json for each of the elements
+			$.getJSON( "data/" + v + ".json", function(data) {
+				// fill the profile with the appropriate content
 				$('#profile').html( 
 					'<h2>' + data.name + '</h2>' +
 					'<p><a href="' + data.pathbrite + '">pathbrite portfolio</a></p>'
 				 );
 			});
 
-
+			// set all the faces 
 			$("#faces li a").css({
-				"background-image" : "url('images/" + v.className + ".jpg')",
-				"background-attachment" : "fixed",
-				"background-repeat" : "none"
+				"background-image" : "url('images/" + v + ".jpg')"
 			});
-
-			return false;
 		});
-	});	
 
-});	
+		// append each element to the base element
+		base_elem.append(elem);
+	});
+};
+
+
+
