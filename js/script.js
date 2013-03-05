5a7c999c3bb4d9cce0427d06d44ba715b7c8f342
Gabe <gabrield@bavc.org>
2013-03-04 17:36:40 -0800

    Added gabe.json, and logic to show when clicked.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 
 $(document).ready(function() {
 
-
+	// reset
 	$('#heading a').click(function() {
+
 		$("#faces li a").css({
 			"background" : "inherit"
 		});
+
+		$('#profile').empty();
+
 	});
 
 
 	var face_lis = $("#faces li");
 	$.each(face_lis, function(i, v) {
+		
 		var link = $(v).find('a');
+
 		$(link).click(function(e) {
+
+			var json_file = "data/" + v.className + ".json";
+
+			$.getJSON( json_file, function(data) {
+				$('#profile').html( 
+					'<h2>' + data.name + '</h2>' +
+					'<p><a href="' + data.pathbrite + '">pathbrite portfolio</a></p>'
+				 );
+			});
+
+
 			$("#faces li a").css({
 				"background-image" : "url('images/" + v.className + ".jpg')",
 				"background-attachment" : "fixed",
 				"background-repeat" : "none"
 			});
+
 			return false;
 		});
 	});	
