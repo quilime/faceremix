2bd9c91629a0e07a8c5c3e4dd4e7ac111fa1d50f
Gabe <gabrield@bavc.org>
2013-03-04 17:03:11 -0800

    Updated face to respond to click.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
+
+$(document).ready(function() {
+
+
+	$('#reset').click(function() {
+		$("#faces li a").css({
+			"background" : "inherit"
+		});
+	});
+
+
 	var face_lis = $("#faces li");
-	$.each(face_lis, function(i, v){
-		$(v).mouseover(function(){
+	$.each(face_lis, function(i, v) {
+		var link = $(v).find('a');
+		$(link).click(function(e) {
 			$("#faces li a").css({
 				"background-image" : "url('images/" + v.className + ".jpg')",
 				"background-attachment" : "fixed",
 				"background-repeat" : "none"
 			});
+			return false;
 		});
-		$(v).mouseout(function(){
-			$("#faces li a").css({
-				"background" : "inherit"
-			});
-		});				
 	});	
+
 });	
